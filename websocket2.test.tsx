bc63685a96ad4c50a5493531d865a1ed0432335c
George Lungley <roloeskimo@gmail.com>
2025-12-31 00:50:28 +0000

    add another file to improve demo

---
+++ websocket2.test.tsx
@@ -0,0 +1,30 @@
+import { expect, test as baseTest, describe } from 'vitest'
+import { default as test, websocketClient } from './mswWorker.js'
+
+describe('validate msw & vitest & websockets (2)', () => {
+  test('once', async ({worker}) => {
+    const {client, port} = websocketClient(worker)
+
+    const openPromise = new Promise((resolve) => {
+      const socket = new WebSocket(`ws://localhost:${port}`)
+      socket.onopen = () => {
+        resolve(true)
+      }
+    })
+
+    await expect(await openPromise).toEqual(true)
+  })
+
+  test('twice', async ({worker}) => {
+    const {client, port} = websocketClient(worker)
+
+    const openPromise = new Promise((resolve) => {
+      const socket = new WebSocket(`ws://localhost:${port}`)
+      socket.onopen = () => {
+        resolve(true)
+      }
+    })
+
+    await expect(await openPromise).toEqual(true)
+  })
+})
